ca27ea90ab9f1a136fd3947c5cf630b1d85b2423
Rafael <RafaelB24@yahoo.com>
2020-04-09 11:45:58 -0700

    Create Posts context, Topic, Comment

---
+++ lib/phoenix_forum/posts/comment.ex
@@ -0,0 +1,16 @@
+defmodule PhoenixForum.Posts.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
---
+++ priv/repo/migrations/20200409182008_create_topics.exs
@@ -0,0 +1,13 @@
+defmodule PhoenixForum.Repo.Migrations.CreateTopics do
+  use Ecto.Migration
+
+  def change do
+    create table(:topics) do
+      add :title, :string 
+      add :content, :string
+
+      timestamps()
+    end
+
+  end
+end
