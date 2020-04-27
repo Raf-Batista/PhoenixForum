8e31ced98e17475ab8c52cdd5d89c8e133a801e5
Rafael <RafaelB2424@gmail.com>
2020-04-26 20:51:04 -0700

    Add user_controller index action, add user/index.html

--- lib/phoenix_forum_web/controllers/user_controller.ex
+++ lib/phoenix_forum_web/controllers/user_controller.ex
@@ -4,9 +4,16 @@ defmodule PhoenixForumWeb.UserController do
     alias PhoenixForum.Accounts
 
     def show(conn, params) do 
-        user = Accounts.get_user(String.to_integer(params["id"]))
-        user = Accounts.get_user_topics_and_comments(user)
+      user = Accounts.get_user(String.to_integer(params["id"]))
+      user = Accounts.get_user_topics_and_comments(user)
 
-        render conn, "show.html", user: user
+      render conn, "show.html", user: user
+    end
+    
+    def index(conn, params) do  
+     user = Accounts.get_user(conn.assigns.user.id)
+     user = Accounts.get_user_topics_and_comments(user)
+  
+     render conn, "index.html", topics: user.topics
     end 
 end 
