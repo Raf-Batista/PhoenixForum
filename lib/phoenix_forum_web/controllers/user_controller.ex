77f0e88b800b7c3d5a1822437f1e36a94c4df2d7
Rafael <RafaelB2424@gmail.com>
2020-04-27 16:33:51 -0700

    Remove user index template and action, add topics to user show

--- lib/phoenix_forum_web/controllers/user_controller.ex
+++ lib/phoenix_forum_web/controllers/user_controller.ex
@@ -1,19 +1,12 @@
 defmodule PhoenixForumWeb.UserController do 
-    use PhoenixForumWeb, :controller 
+  use PhoenixForumWeb, :controller 
 
-    alias PhoenixForum.Accounts
+  alias PhoenixForum.Accounts
 
-    def show(conn, params) do 
-      user = Accounts.get_user(String.to_integer(params["id"]))
-      user = Accounts.get_user_topics_and_comments(user)
+  def show(conn, params) do 
+    user = Accounts.get_user(String.to_integer(params["id"]))
+    user = Accounts.get_user_topics_and_comments(user)
 
-      render conn, "show.html", user: user
-    end
-    
-    def index(conn, params) do  
-     user = Accounts.get_user(conn.assigns.user.id)
-     user = Accounts.get_user_topics_and_comments(user)
-  
-     render conn, "index.html", topics: user.topics
-    end 
+    render conn, "show.html", user: user
+  end
 end 
