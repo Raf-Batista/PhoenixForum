d4799b52eb344bc44dfe96b56fc41a0f7d0c1aa9
Rafael <RafaelB2424@gmail.com>
2020-04-29 19:02:19 -0700

    Add bootstrap toast for error and info messages, add link to create post when logged in

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,9 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+$('.toast').toast({delay: 2000})
+
+$(document).ready(function(){
+    $('.toast').toast('show');
+  });
